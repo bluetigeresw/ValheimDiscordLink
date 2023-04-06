23535c9277639fb34ea6efdac24030b7e6107d00
bluetigeresw <eswwilson@gmail.com>
2023-04-06 02:32:20 +0000

    Add OnDiscordMessageReceived

--- ValheimDiscordLink/ValheimDiscordLink.cs
+++ ValheimDiscordLink/ValheimDiscordLink.cs
@@ -1,9 +1,14 @@
 using BepInEx;
 using HarmonyLib;
+using UnityEngine;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using Discord;
+using Discord.Webhook;
+using System;
+using Valheim;
 
 namespace ValheimDiscordLink
 {
@@ -12,9 +17,20 @@ namespace ValheimDiscordLink
     public class ValheimDiscordLink : BaseUnityPlugin
     {
         private static readonly HttpClient httpClient = new HttpClient();
+        private DiscordWebhookClient _webhookClient;
 
         private void Awake()
         {
+            tring discordWebhookUrl = Config.Bind<string>("General", "DiscordWebhookUrl", "").Value;
+            if (string.IsNullOrEmpty(discordWebhookUrl))
+            {
+                Logger.LogError("DiscordWebhookUrl is not configured.");
+                return;
+            }
+
+            _webhookClient = new DiscordWebhookClient(discordWebhookUrl);
+            _webhookClient.MessageReceived += OnDiscordMessageReceived;
+            
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
             Harmony harmony = new Harmony("com.bluetigeresw.valheimdiscordlink");
             harmony.PatchAll();
@@ -39,6 +55,26 @@ namespace ValheimDiscordLink
             var stringContent = new StringContent(json, Encoding.UTF8, "applications/json");
 
             await httpClient.PostAsync("https://discord.com/api/webhooks/1093329973324038234/3DVJ8AzfQsv8jV-pZX5S5brfmyOaneJkmJfCNatzu9AUHtbJY7Z8f2GQQnPjcdZ_BmS9", stringContent);
+        }
+
+        private async Task OnDiscordMessageReceived(Discord.Webhook.WebhookMessage message)
+        {
+            string username = message.Author.Username;
+            string content = message.Content;
+            DateTime timestamp = message.Timestamp;
+
+            string formattedMessage = $"[Discord] {username}: {content} ({timestamp})";
+            ZNet.instance.Broadcast(formattedMessage);
+        }
+
+        public void OnDestroy()
+        {
+            if (_webhookClient != null)
+            {
+                _webhookClient.MessageReceived -= OnDiscordMessageReceived;
+                _webhookClient.Dispose();
+                _webhookClient = null;
+            }
         }
 
     }
