db2439145d4f48a2cfcf44c2a11eaee0ca334576
bluetigeresw <eswwilson@gmail.com>
2023-04-06 00:26:08 +0000

    Add ValheimDiscordLink.cs

---
+++ ValheimDiscordLink.cs
@@ -0,0 +1,43 @@
+using BepInEx;
+using HarmonyLib;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ValheimDiscordLink
+{
+    [BepInPlugin("com.bluetigeresw.valheimdiscordlink", "Valheim Discord Link", "1.0.0")]
+    pubic class ValheimDiscordLink : BaseUnityPlugin
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("com.bluetigeresw.valheimdiscordlink");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Chat), "InputText")]
+        static class Chat_InputText_Patch
+        {
+            static void Prefix(ref string text)
+            {
+                SendToDiscord(text);
+            }
+        }
+
+        private static async void SendToDiscord(string message)
+        {
+            var payload = new Dictionary<string, string>
+            {
+                { "content", message }
+            };
+            var json = JsonConvert.SerializeObject(payload);
+            var stringContent = new StringContent(json, Encoding.UTF8, "applications/json");
+
+            await httpClient.PostAsync("https://discord.com/api/webhooks/1093329973324038234/3DVJ8AzfQsv8jV-pZX5S5brfmyOaneJkmJfCNatzu9AUHtbJY7Z8f2GQQnPjcdZ_BmS9", stringContent);
+        }
+
+    }
+}
